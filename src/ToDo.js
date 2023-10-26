3a12d6da365f3b3bbe6bc19bc9ed2d34b7c0fb29
Skolnieks <skolnieks.pjvt@pjvt.local>
2023-10-26 09:52:21 +0300

    effect sakums

---
+++ src/ToDo.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function ToDo () {
+    const todo = {
+    userId:1,
+    id: 1,
+    title:"delectus aut autem",
+    completed: false,
+    }
+    return (
+        <>
+   <p></p>
+   <p></p>
+   <p></p>
+   </>
+       )};
+
+    export default ToDo;
